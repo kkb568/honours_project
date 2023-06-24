5f49c2f5713bf0a1d3784bfa858eca3552d041dd
kkb568 <b.koome@alustudent.com>
2023-06-24 19:18:07 +0400

    7th commit

--- auth/emailConfirmSchool.js
+++ auth/emailConfirmSchool.js
@@ -5,30 +5,34 @@ const encryption = require('node-encryption');
 exports.schoolLoginConfirmPage = async(req, res) => {
     // Create verification token.
     const token = jwt.sign({data: req.body.school}, process.env.SCHOOL_CONFIRM_TOKEN, {expiresIn: '10m'});
-    // Create email transporter.
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.AUTH_USER,
-            pass: process.env.AUTH_PASS
-        }
-    });
-    // Send token to the specified email.
-    const mailOptions = {
-        from: "b.koome@alustudent.com",
-        to: req.body.email,
-        subject: "Email verification",
-        text: `Hello Sir/Madam, Please enter the code below to verify your email.
-                Code: ${token}`
-    };
-        
-    transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-            console.log('Email not sent.');
-        } else {
-            console.log('Email sent successfully.');
-        }   
-    });
+    try {
+        // Create email transporter.
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.AUTH_USER,
+                pass: process.env.AUTH_PASS
+            }
+        });
+        // Send token to the specified email.
+        const mailOptions = {
+            from: "b.koome@alustudent.com",
+            to: req.body.email,
+            subject: "Email verification",
+            text: `Hello Sir/Madam, Please enter the code below to verify your email.
+                    Code: ${token}`
+        };
+            
+        transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+                console.log('Email not sent.');
+            } else {
+                console.log('Email sent successfully.');
+            }   
+        });
+    } catch (error) {
+        console.log(console.error);
+    }
 
     const encryptedSchool = encryption.encrypt(req.body.school, process.env.ENCRYPTION_KEY).toString();
     const encryptedEmail = encryption.encrypt(req.body.email, process.env.ENCRYPTION_KEY).toString();
